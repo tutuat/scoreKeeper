771d552d72bafecc726fe2255dfa7bc3b0f15c4c
tutuat <153525161+tutuat@users.noreply.github.com>
2025-05-25 16:37:26 +0200

    initialisation des scores et afficher score max

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const scoreMaxInput = document.getElementById("maxScore");
 const btnPlayer1 = document.getElementById("btnPlayer1");
 const btnPlayer2 = document.getElementById("btnPlayer2");
 const btnReset = document.getElementById("btnReset");
+
+
+let score1 = 0;
+let score2 = 0;
+let scoreMax = scoreMaxInput.value;
+
+scoreMaxDisplay.textContent = scoreMax;
+
+scoreMaxInput.addEventListener("input", function () {
+    scoreMax = this.value;
+    scoreMaxDisplay.textContent = scoreMax;
+  });
