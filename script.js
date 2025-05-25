db8e3e838d4e8ca920bca7cfd6bd85f6d5e10b93
tutuat <153525161+tutuat@users.noreply.github.com>
2025-05-25 16:27:36 +0200

    selection des elemtnts du DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+const score1Display = document.getElementById("score1");
+const score2Display = document.getElementById("score2");
+
+const scoreMaxDisplay = document.getElementById("scoreMaxDisplay");
+const scoreMaxInput = document.getElementById("maxScore");
+
+const btnPlayer1 = document.getElementById("btnPlayer1");
+const btnPlayer2 = document.getElementById("btnPlayer2");
+const btnReset = document.getElementById("btnReset");
